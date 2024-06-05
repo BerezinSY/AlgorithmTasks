fd514f49aa5451b110e9b6bb2ecdcad5d9f35b3f
Stanislav Berezin <stanislav.berezin@icloud.com>
2024-06-05 12:15:59 +0300

    SearchInsertPositionTask

--- AlgorithmTasks_project/Tasks/SearchInsertPositionTask.swift
+++ AlgorithmTasks_project/Tasks/SearchInsertPositionTask.swift
@@ -5,4 +5,36 @@
 //  Created by Stanislav BEREZIN on 05.06.2024.
 //
 
-import Foundation
+class SearchInsertPositionTask {
+    
+    /// На вход подается отсортированный массив и некоторое число
+    ///
+    /// Необходимо вернуть индекс в который необходимо вставить элемент target
+    /// таким образом чтобы порядок сортировки массива не нарушался
+    ///
+    /// **Например:**
+    ///
+    /// Input: nums = [1, 3, 5, 6], target = 2
+    ///
+    /// Output: 1
+    
+    func searchInsert(_ nums: [Int], _ target: Int) -> Int {
+        guard !nums.isEmpty else { return 0 }
+        var lhsBound = 0
+        var rhsBound = nums.indices.count
+        
+        while lhsBound < rhsBound {
+            let midBound = (lhsBound + rhsBound) / 2
+            
+            if target < nums[midBound] {
+                rhsBound = midBound
+            } else if target > nums[midBound] {
+                lhsBound = midBound + 1
+            } else if target == nums[midBound] {
+                return midBound
+            }
+        }
+        
+        return lhsBound
+    }
+}
